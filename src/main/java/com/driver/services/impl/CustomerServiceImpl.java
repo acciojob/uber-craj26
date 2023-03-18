3cbe4ec51cccb2f7b42d1dd8c3c823c8031a5bdc
Arpit <arpitjain7254@gmail.com>
2023-03-18 18:25:19 +0530

    final

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -1,16 +1,13 @@
 package com.driver.services.impl;
 
-import com.driver.model.TripBooking;
+import com.driver.model.*;
 import com.driver.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.driver.model.Customer;
-import com.driver.model.Driver;
 import com.driver.repository.CustomerRepository;
 import com.driver.repository.DriverRepository;
 import com.driver.repository.TripBookingRepository;
-import com.driver.model.TripStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +27,14 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public void register(Customer customer) {
 		//Save the customer in database
+		customerRepository2.save(customer);
 	}
 
 	@Override
 	public void deleteCustomer(Integer customerId) {
 		// Delete customer without using deleteById function
+		Customer customer= customerRepository2.findById(customerId).get();
+		customerRepository2.delete(customer);
 
 	}
 
@@ -43,17 +43,79 @@ public class CustomerServiceImpl implements CustomerService {
 		//Book the driver with lowest driverId who is free (cab available variable is Boolean.TRUE). If no driver is available, throw "No cab available!" exception
 		//Avoid using SQL query
 
+		Driver driver = null;
+
+		List<Driver> driverList = driverRepository2.findAll();
+
+		for (Driver driver1:driverList){
+			if(driver1.getCab().getAvailable()) {
+				if (driver == null || driver.getDriverId() > driver1.getDriverId()) {
+					driver = driver1;
+				}
+			}
+		}
+		if(driver == null){
+			throw new Exception("No cab available!");
+
+		}
+		Customer customer = customerRepository2.findById(customerId).get();
+
+		TripBooking tripBooking = new TripBooking();
+
+		tripBooking.setFromLocation(fromLocation);
+		tripBooking.setToLocation(toLocation);
+		tripBooking.setDriver(driver);
+		tripBooking.setCustomer(customer);
+		tripBooking.setDistanceInKm(distanceInKm);
+		tripBooking.setBill(distanceInKm*driver.getCab().getPerKmRate());
+		tripBooking.setStatus(TripStatus.CONFIRMED);
+
+		customer.getTripBookingList().add(tripBooking);
+
+		driver.getTripBookingList().add(tripBooking);
+
+		customerRepository2.save(customer);
+
+		driverRepository2.save(driver);
+
+		return tripBooking;
+
 	}
 
 	@Override
 	public void cancelTrip(Integer tripId){
 		//Cancel the trip having given trip Id and update TripBooking attributes accordingly
+		TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
+		tripBooking.setStatus(TripStatus.CANCELED);
+		tripBooking.setBill(0);
+		tripBooking.getDriver().getCab().setAvailable(true);
+		tripBookingRepository2.save(tripBooking);
+
+
+
 
 	}
 
 	@Override
 	public void completeTrip(Integer tripId){
 		//Complete the trip having given trip Id and update TripBooking attributes accordingly
+		TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
+		tripBooking.setStatus(TripStatus.COMPLETED);
+		tripBooking.getDriver().getCab().setAvailable(true);
+		tripBookingRepository2.save(tripBooking);
+
+
+
+
 
 	}
 }
+Footer
+		© 2023 GitHub, Inc.
+		Footer navigation
+		Terms
+		Privacy
+		Security
+		Status
+		Docs
+		Contact GitHub
